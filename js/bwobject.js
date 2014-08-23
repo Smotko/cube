6ef54f300bee291456eaef6f98023ac3d494d21c
Anže Pečar <ensmotko@gmail.com>
2014-08-23 22:49:06 +0200

    rotation working in player

--- js/bwobject.js
+++ js/bwobject.js
@@ -5,10 +5,13 @@ define(function(require){
 
   return function BwObject(size, color, is_up) {
     var group = new THREE.Object3D(),
+        invert = false,
+        speed = new THREE.Vector3(),
         cube;
 
     is_up = is_up || Math.random() < 0.5;
     color = color || (is_up ? 0xFFFFFF : 0x000000);
+
     cube = new Cube(color, size);
     if (is_up) {
       cube.position.y = size/2;
@@ -20,11 +23,33 @@ define(function(require){
     return $.extend(cube, {
       update: function(t) {
 
+        if (!invert) {
+          group.rotation.x += speed.z;
+          position.z = original + Math.sin(pivot.rotation.x)*size;
+
+        }
+        else {
+          pivot.rotation.x -= speed;
+          position.z = original - Math.sin(pivot.rotation.x)*size;
+        }
+
+        if (Math.abs(pivot.rotation.x) > Math.PI/2) {
+          pivot.position.z += size;
+          original += size;
+          pivot.rotation.x = 0;
+        }
+
+
+
       },
       // Player is close: turn the object red
       warn: function(distance) {
         cube.material.color.r += distance/10 ;
-      }
+      },
+      invert: function() {
+        invert = !invert;
+      },
+      speed: speed,
     });
   };
 });
--- js/player.js
+++ js/player.js
@@ -7,19 +7,41 @@ define(function(require) {
         pos = size/2,
         bw = new BwObject(size, 0xFF0000, true),
         light = new Light(0xFF0000, 0.5, 5),
-        speed = 1,
-        group = new THREE.Object3D();
-    group.add(light, bw);
+        speed = 0.05,
+        original = -size/2,
+        group = new THREE.Object3D(),
+        position = new THREE.Vector3(0, size/2, -size/2),
+        pivot = new THREE.Object3D();
+    pivot.add(bw);
+    bw.position.z = -size/2;
+    bw.position.y = size/2;
+    group.add(light, pivot);
     return $.extend(group, {
       update: function(t, objects) {
-        bw.position.y = (pos*0.1 + bw.position.y*0.9);
-        bw.position.z += 0.01 * speed;
+        if (pos > 0) {
+          pivot.rotation.x += speed;
+          position.z = original + Math.sin(pivot.rotation.x)*size;
+
+        }
+        else {
+          pivot.rotation.x -= speed;
+          position.z = original - Math.sin(pivot.rotation.x)*size;
+        }
+
+        if (Math.abs(pivot.rotation.x) > Math.PI/2) {
+          pivot.position.z += size;
+          original += size;
+          pivot.rotation.x = 0;
+        }
+
+        position.y =  pos;
+        bw.position.y = pos;
 
         var vector = new THREE.Vector3(0,0,1);
-        var ray = new THREE.Raycaster(bw.position, vector.normalize(), 0, 0.5);
+        var ray = new THREE.Raycaster(position, vector.normalize(), 0, 0.5);
         var intersects = ray.intersectObjects(objects);
         for (var i = 0; i < intersects.length; i++) {
-          if (intersects[i].distance > size/2 * 0.9)
+          if (intersects[i].distance > size/2 * 0.8)
             intersects[i].object.warn(intersects[i].distance);
           else {
             console.log('die');
@@ -27,12 +49,16 @@ define(function(require) {
           }
         }
 
-        light.position.copy(bw.position);
+        light.position.copy(position);
       },
       invert: function(t) {
         pos = pos > 0 ? -size/2 : size/2;
+
+        //original -= Math.sin(pivot.rotation.x)*size
+        //pivot.rotation.x = 0;
+
       },
-      pposition: bw.position
+      pposition: position
     });
   };
 });
